3e43ddd8f4c3b3dcfeac1da476d9ba3d92ecfcd0
Manindra-Roy <anonymous.secret.in@gmail.com>
2024-04-06 22:59:09 +0530

    fixes some bugs and following section added

--- src/script/main.js
+++ src/script/main.js
@@ -48,13 +48,10 @@ const topContents = [
   "New",
   "News",
 ];
-
 let videoTitles =
   "This is the title of this video. Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt, ducimus.";
 let channelNames = "Channel Name";
-
 /////////////////////////////////////////////////////////////
-
 // category data fetch
 for (i = 0; i < topContents.length; i++) {
   document.querySelector(".content-categories").innerHTML += `
@@ -122,6 +119,7 @@ let lCartVideo = document.querySelectorAll(".video-area>a>video");
 let shortVideoSection = document.querySelector(".short-video-section");
 let shortVideo = document.querySelector(".short-video-card video");
 let shortVideoS = document.querySelectorAll(".short-video-card video");
+let followingPage = document.querySelector(".following-page");
 const gotoVideoPlayer = () => {
   contentCategories.setAttribute("style", "display:none;");
   homePageContents.setAttribute("style", "display:none;");
@@ -131,6 +129,16 @@ const gotoVideoPlayer = () => {
   document.querySelectorAll(".suggested-videos .l-cards").forEach((lvCard) => {
     lvCard.remove();
   });
+  document
+    .querySelectorAll(".following-channel-contents .l-cards")
+    .forEach((lvCard) => {
+      lvCard.remove();
+    });
+  document
+    .querySelectorAll(".following-channels .following-channel-profile-card")
+    .forEach((val) => {
+      val.remove();
+    });
 
   for (i = 0; i < videos.length; i++) {
     document.querySelector(
@@ -181,38 +189,37 @@ const gotoVideoPlayer = () => {
     </div>
     </div>`;
   }
-  // set default thambnail on empty thambnail videos
-  document.querySelectorAll(".vid").forEach((val) => {
-    if (val.getAttribute("poster") == "null") {
-      val.setAttribute("poster", "./public/images/d.jpg");
-    }
-  });
-  // set default profile pic on empty profiles
-  document.querySelectorAll(".video-meta .profile-pic").forEach((picval) => {
-    if (picval.getAttribute("style").trim().length == 45) {
-      picval.setAttribute(
-        "style",
-        "background: url(./public/images/d.jpg); background-size: cover;"
-      );
-    }
-  });
+  // set default thambnail on empty thambnail videos and set default profile pic on empty profiles
+  defaultPosterAndProfilePic();
 };
-
+// switch to short video player page
 const gotoShortVideoPlayer = () => {
   contentCategories.setAttribute("style", "display:none;");
   homePageContents.setAttribute("style", "display:none;");
   videoPlayerPage.setAttribute("style", "display:none");
   shortVideoSection.setAttribute("style", "display:grid");
   video.pause();
+  video.setAttribute("src", "");
+  closSideBar();
   document.querySelectorAll(".short-video-card").forEach((svCards) => {
     svCards.remove();
   });
   document.querySelectorAll(".suggested-videos .l-cards").forEach((lvCard) => {
     lvCard.remove();
   });
-  video.setAttribute("src", "");
+  document
+    .querySelectorAll(".following-channel-contents .l-cards")
+    .forEach((lvCard) => {
+      lvCard.remove();
+    });
+  document
+    .querySelectorAll(".following-channels .following-channel-profile-card")
+    .forEach((val) => {
+      val.remove();
+    });
+
   // card content data fetch for short video page
-  for (i = 0; i < videos.length-8; i++) {
+  for (i = 0; i < videos.length - 8; i++) {
     document.querySelector(".short-video-section").innerHTML += `
     <div class="short-video-card">
     <video
@@ -262,13 +269,13 @@ const gotoShortVideoPlayer = () => {
         let onScreenShortVideo = entry.target;
         //
         if (entry.isIntersecting) {
-          console.log(onScreenShortVideo);
+          // console.log(onScreenShortVideo);
           console.log("played");
           onScreenShortVideo.play();
           onScreenShortVideo.muted = !onScreenShortVideo.muted;
           console.log(onScreenShortVideo.muted);
         } else {
-          console.log(onScreenShortVideo);
+          // console.log(onScreenShortVideo);
           console.log("paused");
           onScreenShortVideo.pause();
           onScreenShortVideo.currentTime = 0;
@@ -283,19 +290,19 @@ const gotoShortVideoPlayer = () => {
       val.paused ? val.play() : val.pause();
     });
   }
-  setTimeout(() => {
-    document
-      .querySelector(".short-video-card")
-      .setAttribute("style", "display:none");
-  }, 1);
+  // setTimeout(() => {
+  //   document
+  //     .querySelector(".short-video-card")
+  //     .setAttribute("style", "display:none");
+  // }, 1);
 };
-// switch to home page from video player
-let switchToHomeButtons = document.querySelector(".switch-to-home");
-const gotoHomePage = () => {
-  contentCategories.setAttribute("style", "display:flex;");
-  homePageContents.setAttribute("style", "display:grid;");
+// switch to following page
+const gotoFollowingPage = () => {
+  contentCategories.setAttribute("style", "display:none;");
+  homePageContents.setAttribute("style", "display:none;");
   videoPlayerPage.setAttribute("style", "display:none");
   shortVideoSection.setAttribute("style", "display:none");
+  followingPage.setAttribute("style", "display:grid");
   document.querySelectorAll(".short-video-card").forEach((svCards) => {
     svCards.remove();
   });
@@ -304,8 +311,129 @@ const gotoHomePage = () => {
   });
   video.pause();
   video.setAttribute("src", "");
-};
+  closSideBar();
+  document
+    .querySelectorAll(".following-channel-contents .l-cards")
+    .forEach((lvCard) => {
+      lvCard.remove();
+    });
 
+  document
+    .querySelectorAll(".following-channels .following-channel-profile-card")
+    .forEach((val) => {
+      val.remove();
+    });
+
+  for (i = 10; i < videos.length; i++) {
+    document.querySelector(".following-channels").innerHTML += `
+      <div class="following-channel-profile-card" onclick="refreshFollowingPage(),gotoFollowingPage()">
+      <a href="#">
+        <div
+          class="following-channel-profile-pic"
+          style="
+            background: url(${posters[i]});
+            background-size: cover;
+          "
+        ></div>
+        &nbsp;&nbsp;Channel Name</a
+      >
+    </div>`;
+  }
+
+  // set default thambnail on empty thambnail videos
+  document.querySelectorAll(".following-channel-profile-pic").forEach((val) => {
+    let bGOfProfile = val.style.getPropertyValue("background");
+    if (bGOfProfile.trim().length == 25) {
+      val.style.setProperty("background", "url(./public/images/d.jpg)");
+      val.style.setProperty("background-size", "cover");
+    }
+  });
+
+  for (i = 0; i < videos.length; i++) {
+    document.querySelector(".following-channel-contents").innerHTML += `
+  <div class="l-cards">
+  <div class="video-area">
+    <a href="#" >
+    <video
+    class="vid"
+    onclick="getSrcOfVideo(),gotoVideoPlayer()"
+    poster=${posters[i]}
+    src=${videos[i]}
+    onmouseover="setTimeout(() => {
+      this.play();
+   }, 250);"
+   onmouseout="setTimeout(() => {
+     this.pause();
+     this.currentTime=0;
+     this.load();
+   }, 250);"
+        muted
+        loop
+        preload="none"
+      ></video>
+    </a>
+  </div>
+  <div class="video-meta">
+    <a href="#" class="profile">
+      <div
+        class="profile-pic"
+        style="
+          background: url(${posters[i]});background-size: cover;
+        "
+      ></div>
+    </a>
+    <div class="text-info">
+      <a href="#" class="title">
+      ${videoTitles}
+      </a>
+      <div class="views-and-dates">
+        <a href="#" class="channel-name"
+          >${channelNames}</a
+        >
+        &nbsp;&nbsp;&nbsp;100K Views&nbsp;&nbsp;30 Days Ago
+      </div>
+    </div>
+  </div>
+  </div>`;
+  }
+  // set default thambnail on empty thambnail videos and set default profile pic on empty profiles
+  defaultPosterAndProfilePic();
+};
+// refresh following page
+const refreshFollowingPage = () => {
+  document
+    .querySelectorAll(".following-channel-contents .l-cards")
+    .forEach((val) => {
+      val.remove();
+    });
+};
+// switch to home page
+let switchToHomeButtons = document.querySelector(".switch-to-home");
+const gotoHomePage = () => {
+  contentCategories.setAttribute("style", "display:flex;");
+  homePageContents.setAttribute("style", "display:grid;");
+  videoPlayerPage.setAttribute("style", "display:none");
+  shortVideoSection.setAttribute("style", "display:none");
+  video.pause();
+  video.setAttribute("src", "");
+  closSideBar();
+  document.querySelectorAll(".short-video-card").forEach((svCards) => {
+    svCards.remove();
+  });
+  document.querySelectorAll(".suggested-videos .l-cards").forEach((lvCard) => {
+    lvCard.remove();
+  });
+  document
+    .querySelectorAll(".following-channel-contents .l-cards")
+    .forEach((lvCard) => {
+      lvCard.remove();
+    });
+  document
+    .querySelectorAll(".following-channels .following-channel-profile-card")
+    .forEach((val) => {
+      val.remove();
+    });
+};
 // video transfer engine //////////////
 const getSrcOfVideo = () => {
   document.onclick = (e) => {
@@ -323,22 +451,24 @@ const getSrcOfVideo = () => {
     }
   };
 };
-// set default thambnail on empty thambnail videos
-document.querySelectorAll(".vid").forEach((val) => {
-  if (val.getAttribute("poster") == "null") {
-    val.setAttribute("poster", "./public/images/d.jpg");
-  }
-});
-// set default profile pic on empty profiles
-document.querySelectorAll(".video-meta .profile-pic").forEach((picval) => {
-  if (picval.getAttribute("style").trim().length == 45) {
-    picval.setAttribute(
-      "style",
-      "background: url(./public/images/d.jpg); background-size: cover;"
-    );
-  }
-});
-
+const defaultPosterAndProfilePic = () => {
+  // set default thambnail on empty thambnail videos
+  document.querySelectorAll(".vid").forEach((val) => {
+    if (val.getAttribute("poster") == "null") {
+      val.setAttribute("poster", "./public/images/d.jpg");
+    }
+  });
+  // set default profile pic on empty profiles
+  document.querySelectorAll(".video-meta .profile-pic").forEach((picval) => {
+    if (picval.getAttribute("style").trim().length == 45) {
+      picval.setAttribute(
+        "style",
+        "background: url(./public/images/d.jpg); background-size: cover;"
+      );
+    }
+  });
+};
+defaultPosterAndProfilePic();
 ///////////////////////////////// Finished ///////////////////////////////////
 // menu function //
 const root = document.querySelector(":root");
@@ -363,7 +493,6 @@ const openSideBar = () => {
   sideBar.setAttribute("style", "opacity: 1;");
   menuIcon.setAttribute("onclick", "closSideBar()");
 };
-
 // dark or light function //
 let darkOrLightIcon = document.querySelector(".dark-or-light");
 let darkOrLightIconI = document.querySelector(".dark-or-light>i");
@@ -383,17 +512,21 @@ const switchToDark = () => {
   darkOrLightIconI.setAttribute("class", "ri-sun-fill");
   darkOrLightIcon.setAttribute("onclick", "switchToLight()");
 };
-
-
-
-
-const changeUrl=(nextURL)=>{
-const nextTitle = 'My new page title';
-const nextState = { additionalInformation: 'Updated the URL with JS' };
-// This will create a new entry in the browser's history, without reloading
-window.history.pushState(nextState, nextTitle, nextURL);
-// This will replace the current entry in the browser's history, without reloading
-// window.history.replaceState(nextState, nextTitle, nextURL);
-}
-let nextURL="http://127.0.0.1:5500/index.html"
+const changeUrl = (nextURL) => {
+  const nextTitle = "My new page title";
+  const nextState = { additionalInformation: "Updated the URL with JS" };
+  // This will create a new entry in the browser's history, without reloading
+  window.history.pushState(nextState, nextTitle, nextURL);
+  // This will replace the current entry in the browser's history, without reloading
+  // window.history.replaceState(nextState, nextTitle, nextURL);
+};
+let nextURL = "http://127.0.0.1:5500/index.html";
 // changeUrl(nextURL)
+
+
+
+
+
+// setInterval(() => {
+//   console.clear()
+// }, 10000);
